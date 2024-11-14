801f65630573fe41561cc719a247e999570e7ae3
OleksiiProg <fkorvinn@gmail.com>
2024-11-14 12:59:29 +0200

    Initial commit
     LAB6

---
+++ lib/widgets/signup_screen.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'sing_in_screen.dart';
+void main() => runApp(MaterialApp(
+  home: Home2(),
+));
+
+class Home2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+
+        margin: const EdgeInsets.all(40.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+
+            const Image(
+              width: 210,
+              image: AssetImage('assets/Google-flutter-logo.svg.png'),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Login',
+              ),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Email',
+              ),
+            ),
+            const SizedBox(height: 20),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Password',
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext ctx) {
+                    return const AlertDialog(
+                      title: Text('Message'),
+                      content: Text("Need to implement"),
+                    );
+                  },
+                )
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue[700],
+                foregroundColor: Colors.white,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: const Text('Sign up'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Home(),
+                  ),
+                )
+              },
+              style: ElevatedButton.styleFrom(
+
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.zero,
+                ),
+                minimumSize: const Size(double.infinity, 50),
+              ),
+              child: const Text('Back'),
+            ),
+            const SizedBox(height: 20),
+
+          ],
+        ),
+
+      ),
+
+    );
+  }
+}
